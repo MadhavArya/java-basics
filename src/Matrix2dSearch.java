9a4c4f8c329d9be622bac5fb016c9e2229ef6c4f
Madhav Arya <maarya201@gmail.com>
2025-03-20 17:09:50 +0530

    binary search in 2d array

---
+++ src/Matrix2dSearch.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Matrix2dSearch {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number of rows and columns you want in array");
+        int r=sc.nextInt();
+        int c= sc.nextInt();
+        int [][] arr=new int[r][c];
+        for (int i=0;i<r;i++){
+            System.out.println("Enter elements in "+i+" row");
+            for(int j=0;j<c;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("enter target value");
+        int target= sc.nextInt();
+        int[]res=search(arr,target);
+        System.out.println(Arrays.deepToString(arr));
+        System.out.println("Element found at "+Arrays.toString(res));
+    }
+    static int[] search(int[][]arr,int target){
+        int r=0,c=arr[0].length-1;
+        while(r<arr.length&&c>=0){
+            if(arr[r][c]==target){
+                return new int[]{r,c};
+            }
+            if(arr[r][c]<target){
+                r++;
+            }else{
+                c--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
